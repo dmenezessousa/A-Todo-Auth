fc8470e910c1baefeab1fc09d8941712293750f7
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-10-13 13:00:31 -0400

    Home work

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/user/UserRouter');
+
+var app = express();
+
+mongose.connect(process.env.MONGO_DB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() =>{
+  console.log("MongoDB is Connected");
+}).catch((e)=>{
+  console.log(e);
+})
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({message: "error", err: err.message});
+});
+
+module.exports = app;
---
+++ routes/user/controller/userController.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../model/User');
+const errorHandler = require('../../utils/errorHandler/errorHandler');
+
+
+module.exports = {
+
+};
